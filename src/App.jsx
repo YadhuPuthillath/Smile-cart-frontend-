90925cf416ca0c13e30cb0fbcedfdcbda04ba792
Yadhu Puthillath <kichuputhillath@gmail.com>
2024-07-14 21:01:45 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import PageNotFound from "components/PageNotFound";
+import { Route, Switch, NavLink } from "react-router-dom";
+
 import "./App.css";
+import Home from "./components/Home";
 import Product from "./components/Product";
 
-const App = () => <Product />;
+const App = () => (
+  <>
+    <div className="mx-4 flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route component={Home} path="/" />
+      <Route component={Product} path="/product" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import initializeAxios from "apis/axios";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
@@ -14,7 +15,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
