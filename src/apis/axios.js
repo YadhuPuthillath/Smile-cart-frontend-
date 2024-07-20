c025461f90ac89b10cfaca3d52c7ddc37de72b3b
Yadhu Puthillath <kichuputhillath@gmail.com>
2024-07-20 17:33:25 +0530

    Implemented search functionality to product listing page

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,5 +1,9 @@
-import { keysToCamelCase } from "@bigbinary/neeto-cist";
+import {
+  keysToCamelCase,
+  serializeKeysToSnakeCase,
+} from "@bigbinary/neeto-cist";
 import axios from "axios";
+import { evolve } from "ramda";
 
 const transformResponseKeysToCamelCase = response => {
   if (response.data) response.data = keysToCamelCase(response.data);
@@ -13,16 +17,22 @@ const responseInterceptors = () => {
   });
 };
 
+const requestInterceptors = () => {
+  axios.interceptors.request.use(
+    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  );
+};
+
 const setHttpHeaders = () => {
   axios.defaults.headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
   };
 };
-
 export default function initializeAxios() {
   axios.defaults.baseURL =
     "https://smile-cart-backend-staging.neetodeployapp.com/";
   setHttpHeaders();
   responseInterceptors();
+  requestInterceptors();
 }
