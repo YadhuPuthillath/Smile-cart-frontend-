81faa5d191373549f60ae8d37b3b051462fc7d64
Yadhu Puthillath <kichuputhillath@gmail.com>
2024-07-20 18:20:34 +0530

    Added add to cart feature on the product details page

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
 
 import productsApi from "apis/products";
-import { Header, PageLoader, PageNotFound } from "components/commons";
+import {
+  Header,
+  PageLoader,
+  PageNotFound,
+  AddToCart,
+} from "components/commons";
 import { Typography } from "neetoui";
 import { append, isNotNil } from "ramda";
 import { useParams } from "react-router-dom";
@@ -12,9 +17,7 @@ const Product = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [product, setProduct] = useState({});
-
   const { slug } = useParams();
-
   const fetchProduct = async () => {
     try {
       const response = await productsApi.show(slug);
@@ -30,10 +33,10 @@ const Product = () => {
   useEffect(() => {
     fetchProduct();
   }, []);
+
   const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
-
   if (isLoading) {
     return <PageLoader />;
   }
@@ -62,6 +65,7 @@ const Product = () => {
           <Typography className="font-semibold text-green-600">
             {discountPercentage}% off
           </Typography>
+          <AddToCart {...{ slug }} />
         </div>
       </div>
     </>
