8d33ad0fc7ac7d0138bb0d1fed0c5bf14e345ea1
Yadhu Puthillath <kichuputhillath@gmail.com>
2024-07-25 16:10:17 +0530

    Added pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,36 +4,37 @@ import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
+import { useHistory } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   // const [isLoading, setIsLoading] = useState(true);
   // const [products, setProducts] = useState([]);
   const [searchKey, setSearchKey] = useState("");
-
   const debouncedSearchKey = useDebounce(searchKey);
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+  const history = useHistory();
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
+  const productsParams = {
     searchTerm: debouncedSearchKey,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
 
-  // const fetchProducts = async () => {
-  //   try {
-  //     const data = await productsApi.fetch({ searchTerm: debouncedSearchKey });
-  //     setProducts(data.products);
-  //   } catch (error) {
-  //     console.log("An error occurred:", error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(productsParams);
 
-  // useEffect(() => {
-  //   fetchProducts();
-  // }, [debouncedSearchKey]);
+  const handlePageNavigation = page => {
+    history.replace(
+      buildUrl(routes.products.index, { page, pageSize: DEFAULT_PAGE_SIZE })
+    );
+  };
 
   if (isLoading) {
     return <PageLoader />;
@@ -50,7 +51,10 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchKey}
-            onChange={event => setSearchKey(event.target.value)}
+            onChange={event => {
+              setSearchKey(event.target.value);
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+            }}
           />
         }
       />
@@ -63,6 +67,14 @@ const ProductList = () => {
           ))}
         </div>
       )}
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={handlePageNavigation}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
     </div>
   );
 };
